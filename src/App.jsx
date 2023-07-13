699562a1b84a525867900ee320ad9185551b5d91
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-07-13 19:35:17 +0300

    add IDs & cities & countries to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
@@ -6,9 +8,30 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
 
+
+const URL = "https://my-json-server.typicode.com/MuhammedAbdelnaser/worldwise"
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data)
+      } catch {
+        alert('There was an error loading data...')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities();
+  }, [])
   return (
     <div>
       <BrowserRouter>
@@ -18,9 +41,18 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="countries" element={<p>List of Countries</p>} />
+            <Route
+              index
+              element={<CityList cities={cities}
+                isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities}
+                isLoading={isLoading} />}
+            />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
